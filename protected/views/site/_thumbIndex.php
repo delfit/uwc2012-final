479b8ed41753842af0058f6b23a3793ac3fa69ba
Vinsapphire <>
2012-11-24 16:21:35 +0200

    главная страница

--- protected/views/site/_thumbIndex.php
+++ protected/views/site/_thumbIndex.php
@@ -13,5 +13,32 @@
 				));
 			echo CHtml::closeTag( 'a' );
 			
+			echo CHtml::openTag('ul', array(
+				'class' => 'unstyled',
+				'style' => 'display: block; margin: 1% auto; text-align: center;'
+			));
+				echo CHtml::openTag('li');
+					echo CHtml::openTag('b', array(
+						'style' => 'color: blue;'
+					));
+						echo 'Автор: ';
+					echo CHtml::closeTag('b');
+						echo $data['author'];
+				echo CHtml::closeTag('li');
+				echo CHtml::openTag('li');
+					echo CHtml::openTag('a',  array(
+						'href' => '#',
+						'target' => '_self'
+					));
+						echo CHtml::openTag('b', array(
+							'style' => 'color: blue;'
+						));
+							echo 'Альбом: ';
+						echo CHtml::closeTag('b');
+							echo $data['album'];
+					echo CHtml::closeTag( 'a' );
+				echo CHtml::closeTag('li');
+			echo CHtml::closeTag('ul');
+			
 		echo CHtml::closeTag( 'div' );
 ?>
