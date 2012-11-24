11158077cc45a69249f0b53f4cac94da2e1a3a89
ivan <ivan@probook>
2012-11-24 16:21:16 +0200

    заглушки AccessToken

--- protected/controllers/AlbumController.php
+++ protected/controllers/AlbumController.php
@@ -41,11 +41,7 @@ class AlbumController extends Controller
 	}
 	
 	
-	public function actionList() {
-		// TODO убрать вшитый токен
-		Yii::app()->facebook->fb->setAccessToken( 'AAACEdEose0cBALWR5tGMzoxdz1kzzP1gPtElueKJeYrxNbpqZCKZBXcTZBGtlquxZBM6IUfU7GViV0OI6C2JZAuQv3md71yfsnUKbvw0T3NPq6E9jKJgp' );
-		
-		
+	public function actionList() {		
 		$albumForm = new AlbumForm;
 		
 		if( isset( $_POST[ 'AlbumForm' ] ) ) {
@@ -84,7 +80,7 @@ class AlbumController extends Controller
 			$album[ 'cover' ] = isset( $currentCover[ 'data' ][0]['src_big'] ) ? $currentCover[ 'data' ][0]['src_big'] : 'http://placehold.it/300x200';
 		}
 		
-		
+
 		$this->render( 'list', array( 
 			'albumsProvider' => new CArrayDataProvider( $albums[ 'data' ], array(
 				'id'=>'albums',
@@ -95,6 +91,8 @@ class AlbumController extends Controller
 	
 	public function actionView( $aid ) {
 		//Yii::app()->facebook->fb->setAccessToken( 'AAACEdEose0cBALWR5tGMzoxdz1kzzP1gPtElueKJeYrxNbpqZCKZBXcTZBGtlquxZBM6IUfU7GViV0OI6C2JZAuQv3md71yfsnUKbvw0T3NPq6E9jKJgp' );
+		
+		$aid = '100002102102474_90696';
 		
 		// альбом
 		$albumFql = '
@@ -115,12 +113,17 @@ class AlbumController extends Controller
 			SELECT pid, src_big, src, link, caption, like_info FROM photo WHERE aid= :aid
 		';
 		$photos = Yii::app()->facebook->query( $photosFql, array( ':aid' => $aid ) );
+
+		$photoForm = new PhotoForm();
 		
 		$this->render( 
 			'album', 
 			array(
+				'model' => $photoForm,
 				'album' => $album[ 'data' ],
-				'photos' => new CArrayDataProvider( $photos[ 'data' ] )
+				'photosProvider' => new CArrayDataProvider( $photos[ 'data' ], array(
+					'id'=>'photos',
+				) )
 			)
 		);
 	}
